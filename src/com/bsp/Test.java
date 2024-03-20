fcd2d2f0481a8eeaabebb9637748205d7b00580b
BasavarajBPatil <patilbasavaraj03@gmail.com>
2024-03-20 13:35:43 +0530

    added some extra messages

--- src/com/bsp/Test.java
+++ src/com/bsp/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("sample test message");
 		System.out.println("stmt2");
+		System.out.println("added some exta things to the existing one ");
 	}
 
 }
