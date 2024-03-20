22b8263c61cda883fccc9d26ae466106537ac8ca
BasavarajBPatil <patilbasavaraj03@gmail.com>
2024-03-20 13:42:17 +0530

    comment message added

--- src/com/bsp/Test.java
+++ src/com/bsp/Test.java
@@ -4,7 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("sample test message");
 		System.out.println("stmt2");
-		System.out.println("added some exta things to the existing one ");
+		System.out.println("added some exta things to the existing one ");//comment added just check it ones
+		
 	}
 
 }
